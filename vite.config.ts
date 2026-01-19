7cb14975a99023f287696bd1d6be5b54abe5080b
Yoav Vainrich <yoavain@gmail.com>
2026-01-19 20:54:34 +0200

    ESLint

--- vite.config.ts
+++ vite.config.ts
@@ -25,8 +25,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            "/src": path.resolve(__dirname, "./src"),
-            "@": path.resolve(__dirname, "./src")
+            "/src": path.resolve(__dirname, "./src")
         }
     },
     worker: {
